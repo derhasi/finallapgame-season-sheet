b7bbfcfac7e830dbce34b43ee7ea01a691393089
Johannes Haseitl <johannes@undpaul.de>
2014-02-09 11:59:38 +0100

    Basic main page.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,25 @@
 
 angular.module('finallapgameSeasonSheetApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+
+    $scope.teams = [
+      {
+        name: "Road Avengers",
+        color: "yellow",
+        style: {background: "yellow"},
+        drivers: {
+          red: "Saulo Thao",
+          blue: "Miguel Lopez"
+        },
+        player: ""
+      }
     ];
+
+    $scope.races = [
+      {
+        name: "Race 1"
+      }
+    ];
+
+
   });
