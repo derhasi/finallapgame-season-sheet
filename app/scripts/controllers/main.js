4d4bf3e5fa64cb6bfbbff061467a19ff1ed3f4ac
Johannes Haseitl <johannes@undpaul.de>
2014-02-09 12:53:21 +0100

    Basic styling.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -13,12 +13,25 @@ angular.module('finallapgameSeasonSheetApp')
           blue: "Miguel Lopez"
         },
         player: ""
+      },
+      {
+        name: "Road Avengers",
+        color: "blue",
+        style: {background: "blue"},
+        drivers: {
+          red: "Saulo Thao",
+          blue: "Miguel Lopez"
+        },
+        player: ""
       }
     ];
 
     $scope.races = [
       {
         name: "Race 1"
+      },
+      {
+        name: "Race 2"
       }
     ];
 
